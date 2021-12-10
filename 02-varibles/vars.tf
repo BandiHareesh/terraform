2ca5623c2b1ad005dc7b4234f8695d7e725ff5ee
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-10 10:54:28 +0530

    checking varible function in terraform

--- 02-varibles/vars.tf
+++ 02-varibles/vars.tf
@@ -9,3 +9,13 @@ output "sample" {
 output "sample1" {
   value = "${var.sample} - From Terraform"
 }
+
+// Variable if it is combined with some other string then we have to use ${} interpolation for variable accessing
+
+
+variable "sample2" {}
+
+output "sample2" {
+  value = var.sample2
+}
+
