7cae5de5ad98b6066beb205166e7bd79bdaf2578
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-12 04:28:20 +0530

    roboshop project using terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,52 +1,56 @@
-resource "aws_spot_instance_request" "cheap_worker" {
-  count                     = local.LENGTH
-  ami                       = "ami-0855cab4944392d0a"
-  spot_price                = "0.0034"
-  instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-04bea5344c2dfe413"]
-  wait_for_fulfillment      = true
-  //spot_type                = "persistent"
-  tags                      = {
-    Name                    = element(var.COMPONENTS, count.index)
+//resource "aws_spot_instance_request" "cheap_worker" {
+//  count                       = length(var.COMPONENTS)
+//  ami                         = "ami-0855cab4944392d0a"
+//  spot_price                  = "0.0031"
+//  instance_type               = "t3.micro"
+//  wait_for_fulfillment        = true
+//
+//  tags                        = {
+//    Name                      = element(var.COMPONENTS, count.index)
+//  }
+//}
+
+resource "aws_instance" "sample" {
+  count                       = local.LENGTH
+  ami                         = "ami-0855cab4944392d0a"
+  instance_type               = "t3.micro"
+  vpc_security_group_ids      = ["sg-04bea5344c2dfe413"]
+  tags                        = {
+    Name                      = element(var.COMPONENTS, count.index )
   }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
+resource "aws_route53_record" "records" {
+  count                         = local.LENGTH
+  name                          = element(var.COMPONENTS, count.index )
+  type                          = "A"
+  zone_id                       = "Z020690820L8AJ4UBDKAE"
+  ttl                           = 300
+  records                       = [element(aws_instance.sample.*.private_ip, count.index )]
 }
 
-resource "aws_route53_record" "records" {
-  count                     = local.LENGTH
-  name                      = element(var.COMPONENTS, count.index)
-  type                      = "A"
-  zone_id                   = "Z020690820L8AJ4UBDKAE"
-  ttl                       = 300
-  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
-}
+
+//to connect and run the shell scripting commands
 
 resource "null_resource" "run-shell-scripting" {
-  depends_on                = [aws_route53_record.records]
-  count                     = local.LENGTH
+  depends_on                  = [aws_route53_record.records]
+  count                       = local.LENGTH
   provisioner "remote-exec" {
     connection {
-      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
-      user                  = "centos"
-      password              = "DevOps321"
+      host                    = element(aws_instance.sample.*.public_ip, count.index)
+      user                    = "centos"
+      password                = "DevOps321"
     }
-
-    inline = [
+    inline                   = [
       "cd /home/centos",
-      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
+      "git clone https://github.com/BandiHareesh/shell-scripting.git",
       "cd shell-scripting/roboshop",
-      "git pull",
       "sudo make ${element(var.COMPONENTS, count.index)}"
+
     ]
   }
 }
 
 locals {
-  LENGTH    = length(var.COMPONENTS)
+  LENGTH                      = length(var.COMPONENTS)
 }
--- roboshop-shell-scripting/terraform.tfvars
+++ roboshop-shell-scripting/terraform.tfvars
@@ -1,2 +1,1 @@
 COMPONENTS = ["mysql", "mongodb", "rabbitmq", "redis", "cart", "catalogue", "user", "shipping", "payment", "frontend"]
-COMPONENTS = ["frontend"]
