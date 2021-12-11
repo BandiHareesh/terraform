7111886ab851ca1edfab3d5b9161def1b64f83e6
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-11 19:40:29 +0530

    roboshop project using terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -3,50 +3,8 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                       = "ami-0855cab4944392d0a"
   spot_price                = "0.0034"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-04bea5344c2dfe413"]
   wait_for_fulfillment      = true
-  //spot_type                = "persistent"
   tags                      = {
     Name                    = element(var.COMPONENTS, count.index)
   }
-}
-
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-}
-
-resource "aws_route53_record" "records" {
-  count                     = local.LENGTH
-  name                      = element(var.COMPONENTS, count.index)
-  type                      = "A"
-  zone_id                   = "Z020690820L8AJ4UBDKAE"
-  ttl                       = 300
-  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
-}
-
-resource "null_resource" "run-shell-scripting" {
-  depends_on                = [aws_route53_record.records]
-  count                     = local.LENGTH
-  provisioner "remote-exec" {
-    connection {
-      host                  = element(aws_spot_instance_request.cheap_worker.*.public_ip, count.index)
-      user                  = "centos"
-      password              = "DevOps321"
-    }
-
-    inline = [
-      "cd /home/centos",
-      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting",
-      "cd shell-scripting/roboshop",
-      "git pull",
-      "sudo make ${element(var.COMPONENTS, count.index)}"
-    ]
-  }
-}
-
-locals {
-  LENGTH    = length(var.COMPONENTS)
 }
