41a42ce1eb5dcb56c464e53c35a48ba9144180a8
Bandi Hareesh <bandihareesh7@gmail.com>
2021-12-11 09:00:55 +0530

    roboshop project using terraform

--- 05-modules/ec2/ec2.tf
+++ 05-modules/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   count                 = 2
-  ami                   = "ami-074df373d6bafa625"
+  ami                   = "ami-0855cab4944392d0a"
   instance_type         = var.TYPE
   vpc_security_group_ids = [var.SG_ID]
 
